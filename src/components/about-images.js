f57dae5dab2b6d4f6634c869a06e9b00014fe427
Renaud Lenoir <renaud.lenoir@gmail.com>
2020-04-13 23:16:27 +0200

    Embedded pictures in the 'about' page

--- src/components/about-images.js
+++ src/components/about-images.js
@@ -1,29 +1,83 @@
-import { graphql, useStaticQuery } from "gatsby"
+import React from "react"
+import { useStaticQuery, graphql } from "gatsby"
+import Img from "gatsby-image"
 
-const ImageFragment = useStaticQuery(graphql`
-    fragment ImageFragment on File {
+// const AboutImages = () => {
+  const data = useStaticQuery(graphql`
+    query {
+      bb: file(relativePath: { eq: "BBIT_composed.jpg" }) {
         childImageSharp {
-            fluid(maxHeight: 300) {
-                ...GatsbyImageSharpFluid
-            }
+          fluid(maxHeight: 300) {
+            ...GatsbyImageSharpFluid
+          }
         }
-    }
-`);
+      },
 
-const AboutImages = useStaticQuery(graphql`
-  query {
-    bbitImg: file(relativePath: { eq: "images/BBIT_composed.jpg" }) {
-      ...ImageFragment
-    }
+      serax: file(relativePath: { eq: "VVDA_2018_SERAX_PASSE-PARTOUT_Siska.jpg" }) {
+        childImageSharp {
+          fluid(maxHeight: 300) {
+            ...GatsbyImageSharpFluid
+          }
+        }
+      },
 
-    blImg: file(relativePath: { eq: "images/bl.jpg" }) {
-      ...ImageFragment
-    }
+      franck: file(relativePath: { eq: "Franck_for_Sutherland_composed.png" }) {
+        childImageSharp {
+          fluid(maxHeight: 300) {
+            ...GatsbyImageSharpFluid
+          }
+        }
+      },
 
-    dornbrachtImg: file(relativePath: { eq: "images/12_Gentle.jpg" }) {
-      ...ImageFragment
-    }
-  }
-`);
+      fantini: file(relativePath: { eq: "124_125_MOOD_SANTI_CALECA_2017.jpg" }) {
+        childImageSharp {
+          fluid(maxHeight: 300) {
+            ...GatsbyImageSharpFluid
+          }
+        }
+      },
 
-export default AboutImages;
+      molteni: file(relativePath: { eq: "01-Molteni Paul_01_composed.jpg" }) {
+        childImageSharp {
+          fluid(maxHeight: 300) {
+            ...GatsbyImageSharpFluid
+          }
+        }
+      },
+
+      flos: file(relativePath: { eq: "Infra-Structure_Van Duysen.jpg" }) {
+        childImageSharp {
+          fluid(maxHeight: 300) {
+            ...GatsbyImageSharpFluid
+          }
+        }
+      },
+
+      lenti: file(relativePath: { eq: "Portofino_2017.03_composed.jpg" }) {
+        childImageSharp {
+          fluid(maxHeight: 300) {
+            ...GatsbyImageSharpFluid
+          }
+        }
+      },
+
+      zwilling: file(relativePath: { eq: "ZWILLING_Pro-1_composed.jpg" }) {
+        childImageSharp {
+          fluid(maxHeight: 300) {
+            ...GatsbyImageSharpFluid
+          }
+        }
+      },
+
+      dornbracht: file(relativePath: { eq: "12_Gentle.jpg" }) {
+        childImageSharp {
+          fluid(maxHeight: 300) {
+            ...GatsbyImageSharpFluid
+          }
+        }
+      }
+    }
+  `);
+// }
+
+export function Bbit () { return <Img fluid={data.bbit.childImageSharp.fluid} />; };
