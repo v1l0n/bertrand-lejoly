1ff1396f9b946e93508da4c8c179fbf6988c9280
Renaud Lenoir <renaud_lenoir@eupen.com>
2020-08-07 10:06:19 +0200

    Created about page

--- pages/about.js
+++ pages/about.js
@@ -1,7 +1,7 @@
 import Head from 'next/head'
 import styles from '../styles/About.module.css'
 
-export default function About() {
+export default function About({ projects }) {
     return (
         <div className={styles.container}>
             <Head>
@@ -11,13 +11,34 @@ export default function About() {
 
             <main className={styles.main}>
                 <div className={styles.resume}>
-                    <p>Graduating from Saint-Luc Liège, Belgium in 2002, Bertrand Lejoly started his career first in Germany then in Italy in Milan with Matteo Thun & Partners. Moving back to Belgium in 2013, he took over as head of Vincent Van Duysen's design department, with whom, since nearly seven years he is collaborating on lines for, among others, Molteni&C, B&B Italia, Flos, Kettal, Dada, Paola Lenti and Serax.</p>
-                    <p>In 2019 hes founded is own studio in Antwerp, embarking on a series of collaborations with major brands in the sectors of furniture, lighting, sanitary, as well as on tableware products.</p>
+                    <div>
+                        <p>Graduating from Saint-Luc Liège, Belgium in 2002, Bertrand Lejoly started his career first in Germany then in Italy in Milan with Matteo Thun & Partners. Moving back to Belgium in 2013, he took over as head of Vincent Van Duysen's design department, with whom, since nearly seven years he is collaborating on lines for, among others, Molteni&C, B&B Italia, Flos, Kettal, Dada, Paola Lenti and Serax.</p>
+                        <p>In 2019 hes founded is own studio in Antwerp, embarking on a series of collaborations with major brands in the sectors of furniture, lighting, sanitary, as well as on tableware products.</p>
+                    </div>
+                    <img src="/bl-mugshot.jpg" alt="Bertrand Lejoly" />
                 </div>
-                <div className={styles.mugshot}>
-                    <img src="/bl-mugshot.jpg" alt="Bertrand Lejoly" height="300" />
+                <div>
+                    <p>Excerpt of projects in collaboration with Vincent Van Duysen Architects (2013 until today):</p>
                 </div>
+
+                {projects.map(({ description, image }) => (
+                    <div className={styles.project}>
+                        <img src={"http://localhost:1337" + image.formats.medium.url} />
+                        <p>{description}</p>
+                    </div>
+                ))}
             </main>
         </div>
     )
 }
+
+export async function getStaticProps() {
+    const res = await fetch('http://localhost:1337/projects');
+    const projects = await res.json();
+    
+    return {
+      props: {
+          projects
+      }
+    }
+  }
