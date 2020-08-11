5724fa6614eb175097106b67b6739995bb73e5e4
Renaud Lenoir <renaud_lenoir@eupen.com>
2020-08-11 14:43:36 +0200

    Added responsive css

--- components/layout/layout.js
+++ components/layout/layout.js
@@ -8,6 +8,7 @@ export default function Layout({ children }) {
             <Head>
                 <title>Bertrand Lejoly</title>
                 <link rel="icon" href="/favicon.ico" />
+                <meta name="viewport" content="initial-scale=1.0, width=device-width" />
             </Head>
 
             <Topbar />
